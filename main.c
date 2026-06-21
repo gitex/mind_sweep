8128871b5ce039982e8af60b20a669bf63400bec
Евгений <eanegin@gmail.com>
2026-06-21 19:42:12 +0500

    feat: add progress bar

--- main.c
+++ main.c
@@ -3,16 +3,17 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include "console.h"
 
 #define FILENAME                  "steps.md"
 
 #define H_DELIMITER_SIGN          '-'
-#define H_DELIMITER_WIDTH         32
+#define H_DELIMITER_WIDTH         64
 #define FIRST_ROW                 1
 #define CONSOLE_HELP_LINE         1
-#define OUTPUT_ROW                3
-#define PROGRESS_ROW              5
+#define OUTPUT_ROW                5
+#define PROGRESS_ROW              2
 #define INPUT_ROW                 7
 
 #define OUTPUT_MAX_SIZE           256
@@ -22,10 +23,16 @@
 typedef struct Step {
     char name[64];
     int level;
+    bool is_trigger;
     struct Step *parent;
     struct Step *prev;
     struct Step *next;
 } Step;
+
+typedef struct Buffer {
+    char *data;
+    size_t size;
+} Buffer;
 
 Step *find_next_step(Step *steps, Step *curr_step) {
     // TODO: add check for empty list
@@ -46,24 +53,44 @@ Step *find_next_step(Step *steps, Step *curr_step) {
     return curr;
 }
 
-void fill_output(char *buff, Step *step) {
+void clear_buffer(Buffer *buff) {
+    memset(buff->data, 0, buff->size);
+}
+
+void write_progress(Buffer *buff, int current, int total) {
+    int persent = current * 100 / total;
+
+    strcat(buff->data, "progress: [");
+
+    for (int i = 0; i < persent / 2; i++) {
+        strcat(buff->data, "#");
+    }
+
+    for (int i = 0; i < 50 - persent / 2; i++) {
+        strcat(buff->data, ".");
+    }
+
+    char temp[64];
+    snprintf(temp, sizeof(temp), "] %d/%d", current, total);
+    strcat(buff->data, temp);
+}
+
+void write_step(Buffer *buff, Step *step) {
     if (step == NULL) {
         return;
     }
 
-    memset(buff, 0, sizeof(&buff));
-
     Step *curr = step;
 
     while (curr->parent) {
-        strcat(buff, curr->parent->name);
-        strcat(buff, " > ");
+        strcat(buff->data, curr->parent->name);
+        strcat(buff->data, " > ");
         curr = curr->parent;
     }
 
-    strcat(buff, ANSI_COLOR_GREEN);
-    strcat(buff, step->name);
-    strcat(buff, ANSI_COLOR_RESET);
+    strcat(buff->data, ANSI_COLOR_GREEN);
+    strcat(buff->data, step->name);
+    strcat(buff->data, ANSI_COLOR_RESET);
 }
 
 
@@ -112,6 +139,7 @@ int main(void) {
     int idx = 0;
     int order[10] = {0};
     Step *prev = NULL;
+    int triggers_total = 0;
     while(*start) {
         end = strchr(start, '\n');
         if (end == NULL) {
@@ -119,7 +147,6 @@ int main(void) {
         }
         Step *curr = &steps[idx];
 
-        // trim
         while(*start) {
             if (*start == '*') {
                 curr -> level++;
@@ -139,9 +166,16 @@ int main(void) {
             curr->parent = prev;
         }
 
-        // next
+        // next and previous
         if (prev) {
             prev->next = curr;
+            curr->prev = prev;
+        }
+
+        // is_trigger (prev)
+        if (prev  && prev->next->level >= prev->level) {
+            prev->is_trigger = true;
+            triggers_total++;
         }
 
         start = end + 1;
@@ -152,7 +186,7 @@ int main(void) {
 
     // --- TUI ---
     clear_screen();
-    int on = 1;
+    int quit = 0;
 
     char *commands[2][2] = {
         {"n", "next"},
@@ -160,32 +194,46 @@ int main(void) {
     };
     size_t commands_size = sizeof(commands) / sizeof(*commands);
     Step *curr_step = NULL;
-    char output[OUTPUT_MAX_SIZE];
-    int progress = 1;
+    int progress_current = 0;
+    Buffer progress_buff = {
+        .data = (char[128]){0},
+        .size = 128
+    };
 
-    while(on) {
+    Buffer output_buff = {
+        .data = (char[OUTPUT_MAX_SIZE]){0},
+        .size = OUTPUT_MAX_SIZE
+    };
+
+    while(!quit) {
         move_to_row(CONSOLE_HELP_LINE);
-        printf("Help: ");
+        printf("help: ");
         for (int i = 0; i < commands_size; i++) {
-            print_colored(commands[i][0], ANSI_COLOR_GREEN);
+            print_colored(commands[i][0], ANSI_COLOR_RED);
             printf(" - %s", commands[i][1]);
             if (i != commands_size - 1) { printf(", "); }
         }
-        draw_hline(2, H_DELIMITER_WIDTH, H_DELIMITER_SIGN);
-        draw_hline(4, H_DELIMITER_WIDTH, H_DELIMITER_SIGN);
-        draw_hline(6, H_DELIMITER_WIDTH, H_DELIMITER_SIGN);
+
+        clear_buffer(&progress_buff);
+        write_progress(&progress_buff, progress_current, triggers_total);
+        print_at_row(PROGRESS_ROW, progress_buff.data);
+
+        // draw_hline(4, H_DELIMITER_WIDTH, H_DELIMITER_SIGN);
+        // draw_hline(6, H_DELIMITER_WIDTH, H_DELIMITER_SIGN);
         move_to_row(INPUT_ROW);
 
         char c = getchar();
 
         switch (c) {
-            case 'q': on = 0; break;
+            case 'q': quit = 1; break;
             case 'n':
                 clear_row(INPUT_ROW);
                 clear_row(OUTPUT_ROW);
                 curr_step = find_next_step(steps, curr_step);
-                fill_output(output, curr_step);
-                print_at_row(OUTPUT_ROW, output);
+                clear_buffer(&output_buff);
+                write_step(&output_buff, curr_step);
+                print_at_row(OUTPUT_ROW, output_buff.data);
+                progress_current++;
                 break;
         }
         fflush(stdout);
